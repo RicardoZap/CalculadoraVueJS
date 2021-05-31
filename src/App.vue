7597733772ae50b2a5554d6f93cf1d6a466ba23e
Ricardo Zapata <ricardo.zapata@psa-systems.com>
2021-05-31 18:25:06 -0500

    Calculadora Final

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
@@ -17,6 +14,7 @@ export default {
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,147 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div class="calculator bg-green-500">
+
+  <!-- the input -->
+  <div class="bg-white pt-16 p-5 pb-0 text-black text-right text-3xl">{{ getResult }} </div>
+
+  <!-- the buttons -->
+  <div class="calculator-buttons">
+    <button class="calc-button is-clear bg-red-400" @click="clickAC()">C</button>
+    <button class="calc-button bg-red-600" @click="click('/')">/</button>
+
+    <button class="calc-button bg-red-600" @click="click('7')">7</button>
+    <button class="calc-button bg-red-600" @click="click('8')">8</button>
+    <button class="calc-button bg-red-600" @click="click('9')">9</button>
+    <button class="calc-button bg-red-600" @click="click('x')">*</button>
+
+    <button class="calc-button bg-red-600" @click="click('4')">4</button>
+    <button class="calc-button bg-red-600" @click="click('5')">5</button>
+    <button class="calc-button bg-red-600" @click="click('6')">6</button>
+    <button class="calc-button bg-red-600" @click="click('-')">-</button>
+
+    <button class="calc-button bg-red-600" @click="click('1')">1</button>
+    <button class="calc-button bg-red-600" @click="click('2')">2</button>
+    <button class="calc-button bg-red-600" @click="click('3')">3</button>
+    <button class="calc-button bg-red-600" @click="click('+')">+</button>
+
+    <button class="calc-button is-zero bg-red-600" @click="click('0')">0</button>
+    <button class="calc-button is-equals bg-red-600" @click="clickResult()">=</button>
   </div>
+  
+</div>  
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  
+  data () {
+    return{
+      getOperation: '',
+      getResult: '0',
+    }
+  },
+
+  methods: {
+    clickAC(){
+      this.getResult = '0';
+    },
+    click(num){
+      if(this.getResult == '0'){
+        this.getResult = num;
+      }else{
+        this.getResult += num;
+      }
+    },
+    clickResult(){
+      let operation = this.getResult;
+      this.getOperation = operation;
+
+      operation = operation.replace('x', '*');
+      operation = operation.replace('/', '/');
+      this.getResult = eval(operation);
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+* {
+  box-sizing: border-box;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.calculator input,
+.calc-button   {
+  font-family: 'Space Mono';
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+/* add some spacing */
+body          {
+  background: #F6F6F6;
+  padding-bottom: 30px;
+  padding-top: 30px;  
 }
-a {
-  color: #42b983;
+
+/* limit the width and center */
+.calculator   {
+  max-width: 400px;
+  margin: 0 auto;
+  border: 2px solid #111;
+  border-radius: 5px;
+}
+
+.calculator input   {
+  /* reset basic form styles */
+  border: none;
+  box-shadow: none;
+  outline: none;
+
+  padding: 20px 10px;
+  width: 100%;
+  border-bottom: 2px solid #111;
+  color: #333;
+  text-align: right;
+  font-size: 40px;
+  border-radius: 0;
+}
+
+.calc-button  {
+  border: 1px solid #111; /* black border */
+  padding: 20px;
+  color: #EEE; /* white text */
+  border-radius: 5px; /* rounded corners */
+  font-size: 22px; /* larger fonts */
+  cursor: pointer; /* make it look clickable */
+}
+
+.calculator-buttons {
+  /* small spacing within our container */
+  padding: 20px;
+
+  /* the grid stuff! */
+  display: grid;
+
+  /* create 4 columns */
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+
+  /* add the grid gap */
+  grid-gap: 15px;
+}
+
+/* span across 3 columns */
+.is-zero,
+.is-clear   {
+  grid-column: span 3;
+}
+
+/* go blue */
+.is-clear   {
+  background: #3572DB;
+}
+
+.is-equals {
+  background: #28D060;
 }
 </style>
